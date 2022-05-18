abe0408156e05ff6ace96c85f64ce5417f0a22e7
Antti Ollikkala <antti@huom.io>
2022-05-18 23:45:01 +0300

    rotating shape

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,12 +1,31 @@
 export class RotatingShape {
     shape
     constructor(shape) {
-        this.shape = shape.replace(/[^\S\r\n]+/g, "");;
+        this.shape = shape
+            .split("\n")
+            .map(row => row.trim()
+                .split(''))
     }
 
     toString() {
-        return this.shape + "\n"
+        return this.shape.map(r => r.join('')).join('\n') + '\n'
     }
 
+
+    rotateRight() {
+        const newRotatingShape = new RotatingShape("")
+        newRotatingShape.shape = this.shape.map((r,i) => r.map((c,j) => {
+            return this.shape[(this.shape.length - 1) - j][i]
+        }))
+        return newRotatingShape
+    }
+
+    rotateLeft() {
+        const newRotatingShape = new RotatingShape("")
+        newRotatingShape.shape = this.shape.map((r,i) => r.map((c,j) => {
+            return this.shape[j][(this.shape.length - 1) - i]
+        }))
+        return newRotatingShape
+    }
 
 }
